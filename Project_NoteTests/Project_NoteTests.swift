57ffd76ce4b619ac881f02c80fbd5b8480e4af6a
admin <admin@imac3-7.local>
2017-12-04 10:00:14 +0100

    Initial Commit

---
+++ Project_NoteTests/Project_NoteTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_NoteTests.swift
+//  Project_NoteTests
+//
+//  Created by admin on 04/12/2017.
+//  Copyright © 2017 Quattro. All rights reserved.
+//
+
+import XCTest
+@testable import Project_Note
+
+class Project_NoteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
